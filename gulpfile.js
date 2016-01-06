959059da94c4a543fcca1da316f105ef712798b5
Darius Jazayeri <jazayeri@alum.mit.edu>
2016-01-06 15:56:26 -0800

    Skeleton Open Web App project with a deploy-local task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 
+var LOCAL_OWA_FOLDER = '/path/to/openmrs-standalone-2.3.1/appdataowa/'; // REPLACE THIS
 var THIS_APP_NAME = 'tutorialexample'; // REPLACE THIS
 
 var sources = ['manifest.webapp', '**/*.html', '**/*.js', '**/*.css', '**/*.png',
@@ -12,4 +13,9 @@ gulp.task('default', function () {
     return gulp.src(sources)
         .pipe(zip(THIS_APP_NAME + '.zip'))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy-local', function () {
+    return gulp.src(sources)
+        .pipe(gulp.dest(LOCAL_OWA_FOLDER + THIS_APP_NAME));
 });
